d36aa8d8b80043563af192f1276ecc07d07eb730
Navdeep Natt <nattnavdeep41@gmail.com>
2025-12-20 16:36:07 -0600

    Basic Redirection Capability

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Redirect, routing::get};
 
 #[axum::debug_handler]
-async fn root_handler() -> &'static str {
-    "I have big brain"
+async fn redirect_handler() -> Redirect {
+    Redirect::temporary("http://example.com")
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root_handler));
+    let app = Router::new().route("/redirect", get(redirect_handler));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
