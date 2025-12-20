427f46ada2a2815b4b80883e9df0aac1e55e4dec
Navdeep Natt <nattnavdeep41@gmail.com>
2025-12-20 14:54:24 -0600

    Implemented basic axum server in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {"I have big brain"}));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to bind");
+    axum::serve(listener, app).await.expect("Failed to initialize application");
 }
