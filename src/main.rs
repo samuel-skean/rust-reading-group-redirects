525f79d9811dadc85ba3e528420a21bef6d41929
Samuel Skean <sskean2@uic.edu>
2025-12-20 15:20:41 -0600

    Factored out handler code.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{
-    routing::get,
-    Router
-};
+use axum::{Router, routing::get};
+
+#[axum::debug_handler]
+async fn root_handler() -> &'static str {
+    "I have big brain"
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {"I have big brain"}));
+    let app = Router::new().route("/", get(root_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to bind");
-    axum::serve(listener, app).await.expect("Failed to initialize application");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to bind");
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to initialize application");
 }
